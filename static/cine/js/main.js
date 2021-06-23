2d931ca3b942edceac3ed40518ab15f18ffd2f45
IvetteRojas <81447583+IvetteRojas@users.noreply.github.com>
2021-06-23 15:18:11 -0400

    Add files via upload

---
+++ static/cine/js/main.js
@@ -0,0 +1,98 @@
+function App() {}
+
+window.onload = function(event) {
+    var app = new App();
+    window.app = app;
+};
+
+App.prototype.processingButton = function(event) {
+    const btn = event.currentTarget;
+    const slickList = event.currentTarget.parentNode;
+    const track = event.currentTarget.parentNode.querySelector('#track');
+    const slick = track.querySelectorAll('.slick');
+
+    const slickWidth = slick[0].offsetWidth;
+
+    const trackWidth = track.offsetWidth;
+    const listWidth = slickList.offsetWidth;
+
+    track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+
+    btn.dataset.button == "button-prev" ? prevAction(leftPosition, slickWidth, track) : nextAction(leftPosition, trackWidth, listWidth, slickWidth, track)
+}
+
+let prevAction = (leftPosition, slickWidth, track) => {
+    if (leftPosition > 0) {
+        console.log("entro 2")
+        track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
+    }
+}
+
+let nextAction = (leftPosition, trackWidth, listWidth, slickWidth, track) => {
+    if (leftPosition < (trackWidth - listWidth)) {
+        track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
+    }
+}
+
+/* ---------------formulario contacto---------------*/
+const form = document.getElementById('form');
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const telefono = document.getElementById('telefono');
+const comentario = document.getElementById('comentario');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    // trim to remove the whitespaces
+    const nombreValue = nombre.value.trim();
+    const emailValue = email.value.trim();
+    const telefonoValue = telefono.value.trim();
+    const comentarioValue = comentario.value.trim();
+
+    if (nombreValue === '') {
+        setErrorFor(nombre, 'No puede dejar el nombre en blanco');
+    } else {
+        setSuccessFor(nombre);
+    }
+
+    if (emailValue === '') {
+        setErrorFor(email, 'No puede dejar el email en blanco');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'No ingreso un email válido');
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (telefonoValue === '') {
+        setErrorFor(telefono, 'No puede dejar el telefono en blanco');
+    } else {
+        setSuccessFor(telefono);
+    }
+
+    if (comentarioValue === '') {
+        setErrorFor(comentario, 'No puede dejar comentario en blanco');
+    } else {
+        setSuccessFor(comentario);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
