62f108a01d23188df8b2ddd5591bb302cb91662e
IvetteRojas <81447583+IvetteRojas@users.noreply.github.com>
2021-06-23 15:36:03 -0400

    Add files via upload

---
+++ views.py
@@ -0,0 +1,218 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'cine/index.html')
+
+def estrenos(request):
+    return render(request, 'cine/estrenos.html')
+
+def contacto(request):
+    return render(request, 'cine/contacto.html')
+
+def peliculas(request):
+    return render(request, 'cine/pelis.html')
+
+def proxestrenos(request):
+    return render(request, 'cine/proxestrenos.html')
+
+def series(request):
+    return render(request, 'cine/series.html')
+
+def godzilla(request):
+    return render(request, 'cine/aa.html')
+
+def maravilla(request):
+    return render(request, 'cine/ab.html')
+
+def ava(request):
+    return render(request, 'cine/ava.html')
+
+def avpresa(request):
+    return render(request, 'cine/avpresa.html')
+
+def batman(request):
+    return render(request, 'cine/bat.html')
+
+def bob(request):
+    return render(request, 'cine/bob.html')
+
+def bosque(request):
+    return render(request, 'cine/bosque.html')
+
+def chicas(request):
+    return render(request, 'cine/chicas.html')
+
+def fragmentos(request):
+    return render(request, 'cine/frag.html')
+
+def cazadores(request):
+    return render(request, 'cine/gre.html')
+
+def invisible(request):
+    return render(request, 'cine/hom.html')
+
+def brujas(request):
+    return render(request, 'cine/jov.html')
+
+def justice(request):
+    return render(request, 'cine/justice.html')
+
+def greenland(request):
+    return render(request, 'cine/mundo.html')
+
+def presencia(request):
+    return render(request, 'cine/pr.html')
+
+def voces(request):
+    return render(request, 'cine/voces.html')
+
+
+#Series
+def crown(request):
+    return render(request, 'cine/crow.html')
+
+def dark(request):
+    return render(request, 'cine/dark.html')
+
+def dash(request):
+    return render(request, 'cine/dash.html')
+
+def ojos(request):
+    return render(request, 'cine/det.html')
+
+def ginny(request):
+    return render(request, 'cine/ginny.html')
+
+def haunting(request):
+    return render(request, 'cine/haunting.html')
+
+def hollywood(request):
+    return render(request, 'cine/hollywood.html')
+
+def lupin(request):
+    return render(request, 'cine/lupin.html')
+
+def night(request):
+    return render(request, 'cine/night.html')
+
+def ottoman(request):
+    return render(request, 'cine/ottoman.html')
+
+def perdida(request):
+    return render(request, 'cine/perd.html')
+
+def revolucion(request):
+    return render(request, 'cine/revolucion.html')
+
+def stranger(request):
+    return render(request, 'cine/stranger.html')
+
+def tribu(request):
+    return render(request, 'cine/tribu.html')
+
+def walking(request):
+    return render(request, 'cine/walking.html')
+
+def witcher(request):
+    return render(request, 'cine/witcher.html')
+
+#estrenos
+def assis(request):
+    return render(request, 'cine/assis.html')
+
+def cace(request):
+    return render(request, 'cine/cace.html')
+
+def cielo(request):
+    return render(request, 'cine/cielo.html')
+
+def cuidado(request):
+    return render(request, 'cine/cuidado.html')
+
+def emma(request):
+    return render(request, 'cine/emma.html')
+
+def fuga(request):
+    return render(request, 'cine/fuga.html')
+
+def hunt2(request):
+    return render(request, 'cine/hunt2.html')
+
+def kings(request):
+    return render(request, 'cine/kings.html')
+
+def lobo(request):
+    return render(request, 'cine/lobo.html')
+
+def mulan(request):
+    return render(request, 'cine/mulan.html')
+
+def mutants(request):
+    return render(request, 'cine/mutants.html')
+
+def orden(request):
+    return render(request, 'cine/orden.html')
+
+def tenet(request):
+    return render(request, 'cine/tenet.html')
+
+def vast(request):
+    return render(request, 'cine/vast.html')
+
+def venganza(request):
+    return render(request, 'cine/ven.html')
+
+def xico(request):
+    return render(request, 'cine/xico.html')
+
+#proxestrenos
+def antlers(request):
+    return render(request, 'cine/antlers.html')
+
+def black(request):
+    return render(request, 'cine/black.html')
+
+def born(request):
+    return render(request, 'cine/born.html')
+
+def conjuro3(request):
+    return render(request, 'cine/conjuro3.html')
+
+def dune(request):
+    return render(request, 'cine/dune.html')
+
+def escape(request):
+    return render(request, 'cine/escape.html')
+
+def friend(request):
+    return render(request, 'cine/friend.html')
+
+def hitmans(request):
+    return render(request, 'cine/hitmans.html')
+
+def kombat(request):
+    return render(request, 'cine/kombat.html')
+
+
+def mym(request):
+    return render(request, 'cine/malcom.html')
+
+def marksman(request):
+    return render(request, 'cine/marksman.html')
+
+def prin(request):
+    return render(request, 'cine/prin.html')
+
+def space(request):
+    return render(request, 'cine/space.html')
+
+def time(request):
+    return render(request, 'cine/time.html')
+
+def venom2(request):
+    return render(request, 'cine/venom.html')
+
+def samaritan(request):
+    return render(request, 'cine/samaritan.html')
